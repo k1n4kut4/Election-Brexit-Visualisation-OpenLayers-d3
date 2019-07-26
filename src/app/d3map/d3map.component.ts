0333dd5c5a26366bdd0ee77e6b6ffbdaa711b7e6
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-27 00:12:33 +0100

    svg with circle - testing- success

--- src/app/d3map/d3map.component.ts
+++ src/app/d3map/d3map.component.ts
@@ -18,4 +18,24 @@ export class D3mapComponent implements OnInit {
       d3.select('p').style('color', 'red');
   }
 
+  radius: number=15;
+  clickeventcount: number =0 ;
+  svgclickhandle(event: any ) {
+    this.clickeventcount+=1;
+    let colorname = 'black';
+  if (this.clickeventcount % 5 === 0)  {
+      colorname ='red';
+  } else if (this.clickeventcount % 3 === 0) {
+    colorname ='yellow';
+  } else if (this.clickeventcount % 2 === 0) {
+    colorname ='purple';
+  }
+  d3.select(event.target).
+    append('circle').
+    attr('cx', event.x).
+    attr('cy',event.y).
+    attr('r',this.radius).
+    attr('fill',colorname);
+  }
+
 }
