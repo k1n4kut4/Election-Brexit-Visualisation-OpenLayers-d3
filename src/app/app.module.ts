03908e3dc97902b85ec8ed850798848aa973c07f
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-07-27 00:03:51 +0100

    new component d3map

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { D3mapComponent } from './d3map/d3map.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    D3mapComponent
   ],
   imports: [
     BrowserModule,
