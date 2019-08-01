e5c637d799bd72616aebbb28cf03b67818d6d5d6
Lucius Mathematica <52294958+k1n4kut4@users.noreply.github.com>
2019-08-01 02:43:51 +0100

    rxjs for array

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -1,25 +1,54 @@
 import { Injectable } from '@angular/core';
+import { HttpClient, HttpErrorResponse, HttpParams } from '@angular/common/http';
+import { Observable, throwError } from 'rxjs'; 
+import { map, catchError } from 'rxjs/operators'; 
+import { of } from 'rxjs';
 
 import * as ConstitData from '../assets/json/mp_details_full.json'; 
+
+export class ConstitDataFields {
+  constructor(
+    code_ons: any,
+    constit_name: any,
+    region: any,
+    mp_img: any,
+    mp_name: any,
+    electorate_size: any, 
+    votes: any,
+    status: any,
+    win: any,
+    swing: any,
+    year: any,
+    con: any,
+    lab: any,
+    lib: any,
+    grn: any,
+    dup: any,
+    sf: any,
+    snp: any,
+    ukip: any,
+    uup: any,
+    others: any) {}
+}
 
 @Injectable({
   providedIn: 'root'
 })
 export class DataService {
 
-  constructor() { }
+  constitData: ConstitDataFields[];
 
-  getConstitData(id){
+  constructor(private http: HttpClient) { }  
 
-    console.log(id);
+  getConstitData(id): Observable<ConstitDataFields[]> {
 
-    const cd = ConstitData;//.default;  
+    const cd = ConstitData;//.default;        
+    //this.constitData["constit_name"] = cd[id]["constit"];
+    //console.log(this.constitData); 
 
-    console.log(cd);
-
-    console.log(cd[id]);
-
-    return cd[id];
+    this.constitData = cd;
+    
+    return of(this.constitData);
 
   }
 }
